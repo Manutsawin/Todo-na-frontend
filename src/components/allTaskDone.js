376d7be88c26ae373ddf9490470b830a4c949609
SK-Pupae <SK-Pupae@DESKTOP-BECBDNN>
2021-04-17 23:24:44 +0700

    test

--- src/components/allTaskDone.js
+++ src/components/allTaskDone.js
@@ -1,25 +1,23 @@
 import React from 'react'
 import axios from 'axios'
-import "./style/Todo.css";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faTimesCircle,faCheckCircle,faTrash,faPencilAlt,faCheckSquare,faTimes,faFile} from '@fortawesome/free-solid-svg-icons'
+import {faTimesCircle,faTrash,faPencilAlt,faCheckSquare,faTimes,faFile} from '@fortawesome/free-solid-svg-icons'
+import "./style/Todo.css";
 
 
-const iconCheck = <FontAwesomeIcon size="2x" color="green" icon={faCheckCircle} />
+const iconClose = <FontAwesomeIcon size="2x" color="red" icon={faTimesCircle} />
 const iconTrash = <FontAwesomeIcon size="2x" color="grey" icon={faTrash} />
 const iconEdit = <FontAwesomeIcon size="2x" color="orange" icon={faPencilAlt} />
 const iconSave = <FontAwesomeIcon size="3x" color="green" icon={faCheckSquare} />
 const iconCancel = <FontAwesomeIcon size="3x" color="red" icon={faTimes} />
 const iconTask = <FontAwesomeIcon size="7x" color="grey" icon={faFile} />
 
-const AllTaskTodo = ()=>{
-
-    const [todo,setTodo] = React.useState([]);
-    const TokenLocal = localStorage.getItem("token")
-    
-    const [empty,setEmpty]= React.useState(false);
-
+const AllTaskDone = ()=>{
+   
+    const [done, setDone] = React.useState([]);
     const [edit, setEdit] = React.useState(false);
+
+    const [empty,setEmpty]= React.useState(false);
     
     const [editData,setEditData] = React.useState({
         ID:"",
@@ -27,37 +25,38 @@ const AllTaskTodo = ()=>{
         Time : ""
     });
     
-    
+    const TokenLocal = localStorage.getItem("token")
+
+     
     React.useEffect(()=>{
         fetchTask(); 
     }, []);
 
     function fetchTask(){
-        getTaskFalse();
-
+        getTaskTrue();
+        
     }
-    
-    function  getTaskFalse(){
+
+    function  getTaskTrue(){
         axios
-            .get(`https://todo-na-backend.herokuapp.com/api/TaskAdmin?token=${TokenLocal}&isFinished=false`)
+            .get(`https://todo-na-backend.herokuapp.com/api/TaskAdmin?token=${TokenLocal}&isFinished=true`)
             .then((res)=>{
                 
                 if(res.data=="")
                 {
                     setEmpty(true)
                 }
-                setTodo(res.data)
-                
+                setDone(res.data)
                 
             });
     }
-
 
     function  onUpdate(id,isFinished){
         axios.put(`https://todo-na-backend.herokuapp.com/api/task?token=${TokenLocal}&id=${id}`,{
             isFinished : isFinished,
         }).then(()=>{
-            fetchTask();
+            
+            fetchDoneTask();
         })
     }
 
@@ -67,36 +66,33 @@ const AllTaskTodo = ()=>{
             time : Time
         }).then(()=>{
             
-            fetchTask();
+            fetchDoneTask();
         })
     }
 
+
     function onDelete(id){
         axios.delete(`https://todo-na-backend.herokuapp.com/api/task?token=${TokenLocal}&id=${id}`)
-        .then(()=>{
-            fetchTask();
+        .then(()=>{ 
+            fetchDoneTask();
         });
     }
-    
-    return <div>
-       
+
+    return<div>
         <div>
-
-
-
+            
             <ul className="mt-lg-5">
-
-
-                {
-                    edit ?
-                        (
+                
+            {
+             edit ? 
+            (
                             <div>
+                                
                                 <center>
 
-                                    <h1 className="mb-md-5" style={{ fontFamily: "Nunito" }}  >Edit</h1>
+                                    <h1 className="mb-md-5" style={{fontFamily:"Nunito"}}  >Edit</h1>
 
                                 </center>
-
                                 <center>
                                     <div className="col-md-6">
                                         <div className="Card mb-sm-2" >
@@ -104,19 +100,19 @@ const AllTaskTodo = ()=>{
                                                 <div className="col-md-6">
                                                     <div className="row">
                                                         <input type="User" className="form-control" placeholder="Enter User" value={editData.Name} onChange={(e) => {
-                                                            setEditData({
-                                                                ID: editData.ID,
-                                                                Name: e.target.value,
-                                                                Time: editData.Time
+                                                            setEditData ({
+                                                                ID : editData.ID,
+                                                                Name : e.target.value,
+                                                                Time : editData.Time
                                                             });
                                                         }} />
                                                     </div>
                                                     <div className="row">
                                                         <input className="form-control" value={editData.Time} type="time" onChange={(e) => {
-                                                            setEditData({
-                                                                ID: editData.ID,
-                                                                Name: editData.Name,
-                                                                Time: e.target.value
+                                                            setEditData ({
+                                                                ID : editData.ID,
+                                                                Name : editData.Name,
+                                                                Time : e.target.value
                                                             });
                                                         }} />
                                                     </div>
@@ -126,13 +122,13 @@ const AllTaskTodo = ()=>{
                                                         <div className="col-sm-6" >
 
                                                         </div>
-                                                        <div className="col-sm-2 mr-md-2 ml-md-2" >
-                                                            <div align="right" className="buttonGrey" onClick={() => {
-                                                                onUpdateData(editData.ID, editData.Name, editData.Time)
+                                                        <div className="col-sm-2  mr-md-2 ml-md-2" >
+                                                            <div align="right" className="buttonGrey" onClick={() =>{
+                                                                onUpdateData(editData.ID,editData.Name,editData.Time)
                                                                 setEdit(false)
                                                             }}>{iconSave}</div>
                                                         </div>
-                                                        <div className="col-sm-2 mr-md-2 ml-md-2" >
+                                                        <div className="col-sm-2  mr-md-2 ml-md-2" >
                                                             <div align="right" className="buttonGrey" onClick={() => setEdit(false)}>{iconCancel}</div>
                                                         </div>
                                                     </div>
@@ -149,21 +145,23 @@ const AllTaskTodo = ()=>{
                         )
                         :
                         (
+
                             <div>
+
                                 <center>
 
-                                    <h1 className="mb-md-5" style={{ fontFamily: "Nunito" }}  >Todo</h1>
+                                    <h1 className="mb-md-5" style={{fontFamily:"Nunito"}}  >Done</h1>
                                     {
                                         empty ?
                                             (
                                                 <div>
                                                     {iconTask}
-                                                    <h4 className="mt-md-5">All tasks completed!</h4>
-                                                    <h6 style={{ color: "grey", }}>You can create new tasks</h6>
-
+                                                    <h4 className="mt-md-5">All tasks clear!</h4>
+                                                    <h6 style={{color: "grey",}}>You can create new tasks</h6>
+                                                    
                                                 </div>
-
-
+                                                
+                                                
                                             )
                                             :
                                             (
@@ -171,10 +169,10 @@ const AllTaskTodo = ()=>{
                                             )
                                     }
 
-
                                 </center>
 
-                                {todo.map((task) => (
+
+                                {done.map((task) => (
                                     <center>
                                         <div className="col-md-6">
                                             <div className="Card mb-sm-2" >
@@ -193,17 +191,18 @@ const AllTaskTodo = ()=>{
 
                                                             </div>
                                                             <div className="col-sm-2" >
-                                                                <div align="right" className="buttonGrey" onClick={() => onUpdate(task._id, true)}> {iconCheck}</div>
+                                                                <div align="right" className="buttonGrey" onClick={() => onUpdate(task._id, false)}> {iconClose}</div>
                                                             </div>
                                                             <div className="col-sm-2" >
                                                                 <div align="right" className="buttonGrey" onClick={() => {
-                                                                    setEdit(true);
-                                                                    setEditData({
-                                                                        ID: task._id,
-                                                                        Name: task.taskName,
-                                                                        Time: task.time
-                                                                    });
 
+                                                                    setEdit(true);
+                                                                    setEditData ({
+                                                                        ID : task._id,
+                                                                        Name : task.taskName,
+                                                                        Time : task.time
+                                                                    });
+                                                                    
                                                                 }}> {iconEdit}</div>
                                                             </div>
                                                             <div className="col-sm-2" >
@@ -213,22 +212,22 @@ const AllTaskTodo = ()=>{
                                                     </div>
 
                                                 </div>
+
+
+
                                             </div>
                                         </div>
                                     </center>
 
                                 ))}
                             </div>
-                        )
-                }
+            )
+            }
+
+               
+                
             </ul>
+          
         </div>
-
-
-        
-      
-       
     </div>;
-    
 }
-export default AllTaskTodo;
