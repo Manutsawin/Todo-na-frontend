10390426ed5e71a8ddc1d5944470f49b424a4cb4
SK-Pupae <SK-Pupae@DESKTOP-BECBDNN>
2021-04-17 19:57:42 +0700

    testcheck

--- src/components/access.js
+++ src/components/access.js
@@ -9,20 +9,67 @@ import Navbar from "./Nav/NavbarLogined"
 
 
 const Access = ()=>{
+
+  const [Session,setSession] = useState({
+    isAdmin : false ,
+  });
+
+  const TokenLocal = localStorage.getItem("token")
+
+  function  checkAdmin(){
+      
+    axios
+      .post(`https://todo-na-backend.herokuapp.com/api/isAdmin?token=${TokenLocal}`)
+      .then((res)=>{
+         if(res.data==true)
+         {
+          setSession ({
+            isAdmin : true
+          })
+         }
+         else{
+          localStorage.setItem("isLogined",false) 
+          setSession ({
+            isAdmin : false
+          })
+         }
+      })
+    
+
+  }
+
+  React.useEffect(()=>{
+    checkAdmin();
+  }, []);
+
+
     return<div>
-      <Navbar/>
-      <Router>
-        <Switch>
-          <Route exact path="/" component={() =>  <Profile />} />
-          <Route path="/todo" component={() =>  <Todo />}/>
-          <Route path="/done" component={() =>  <Done />}/>
-          <Route path="/create" component={() =>  <Create />}/>
-          <Route path="/Profile" component={() =>  <Profile />}/>
-          <Route path="/contact" component={() =>  <Contact />}/>
-          <Route path="/signIn" component={() =>  <Profile />}/>
-          <Route  component={() =>  <Todo />}/>
-        </Switch> 
-      </Router>
+      {
+        isAdmin ? 
+        (
+          <Contact/>
+        )  
+        : 
+        (
+          <div>
+              <Navbar/>
+              <Router>
+                <Switch>
+                  <Route exact path="/" component={() =>  <Profile />} />
+                  <Route path="/todo" component={() =>  <Todo />}/>
+                  <Route path="/done" component={() =>  <Done />}/>
+                  <Route path="/create" component={() =>  <Create />}/>
+                  <Route path="/Profile" component={() =>  <Profile />}/>
+                  <Route path="/contact" component={() =>  <Contact />}/>
+                  <Route path="/signIn" component={() =>  <Profile />}/>
+                  <Route  component={() =>  <Todo />}/>
+                </Switch> 
+              </Router>
+          </div>
+          
+        )
+      }
+      
     </div>;
 }
 export default Access;
